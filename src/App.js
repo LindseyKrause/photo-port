7e4c7b77dccf050558e3424a2819c140e7902014
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-07-20 18:49:23 -0700

    added gallery components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import About from './components/About';
+import Gallery from './components/Gallery';
 import Nav from './components/Nav'
-
 function App() {
 
   return (
     <div>
       <main>
         <Nav></Nav>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
