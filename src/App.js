42b7b23f2710f7e8c5cf6608d3eea1271882082a
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-07-24 17:13:14 -0700

    added missing gallery components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import About from './components/About';
-import Nav from './components/Nav'
-
+import Nav from './components/Nav';
+import ContactForm from './components/Contact';
+import Gallery from './components/Gallery'
 function App() {
 
   return (
     <div>
       <main>
         <Nav></Nav>
+        <ContactForm></ContactForm>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
